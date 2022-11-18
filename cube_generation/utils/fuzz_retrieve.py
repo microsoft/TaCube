73eac757499c953477bb4d6de9c62d40ac65af2f
Haoyu Dong <42404535+HaoAreYuDong@users.noreply.github.com>
2022-11-18 08:54:57 +0800

    Update fuzz_retrieve.py

--- cube_generation/utils/fuzz_retrieve.py
+++ cube_generation/utils/fuzz_retrieve.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
